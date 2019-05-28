f1e934339bb8c0a4454ba90eee06731de01c5dbd
Xavier Martin <nolazybits+github@gmail.com>
2019-05-29 11:02:57 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,62 +18,34 @@ brew update
 # Upgrade any already installed formulae
 brew upgrade
 
-# install cake brew for the one fearing the terminal
-brew cask install cakebrew
-
-# install mas - install App Store app from command line
-if test ! $(which brew)
-then
-  echo "  Installing MAS for you."
-  brew install mas
-fi
-
-# John background switcher
-mas install 907640277
-
 # install the versions tap (beta/snapshot channel)
 brew tap caskroom/versions
 
-# path finder explorer (replace finder)
-brew cask install path-finder
+# APPLICATIONS
 
-# iterm2 terminal
-brew cask install iterm2
-
-# vivaldi-snapshot browser
-brew cask install vivaldi-snapshot
-
-# vim editor
-brew install vim
-
-# keeweb password manager
-brew cask install keeweb
-
-# spotify
-brew cask install spotify
+# alfred, replace search
+brew cask install alfred
 
 # android file transfer
 brew cask install android-file-transfer
 
-# bear notes - id comes from mas search bear
-mas install 1091189122
-
-# gravit designer
-mas install 1207744923
-
 # canary email client
 brew cask install canary
+
+# install cake brew for the one fearing the terminal
+brew cask install cakebrew
 
 # charles web proxy
 brew cask install charles
 
+# realpath needed for the dotfiles install
+brew install coreutils
+
 # docker
-brew cask install docker
-# now run the app
+brew cask install docker-edge
 
 # dropbox
 brew cask install dropbox
-# now run the app
 
 # etcher - image burner
 brew cask install etcher
@@ -81,47 +53,111 @@ brew cask install etcher
 # flux - cooler warms as dark comes
 brew cask install flux
 
-# postman
-brew cask install postman
+# gif maker
+brew cask install gifox
 
-# install spectacle window manager
-brew cask install spectacle
+# google drive auto backup
+brew cask install google-backup-and-sync
 
-# franz messaging app aggregator
-brew cask install franz
+# google chrome browser
+brew cask install google-chrome
 
-# archives 
-brew cask install the-unarchiver
+# insomnia rest client
+brew cask install insomnia
 
-# send videos to Apple TV or chrome cast
-brew cask install beamer
+# intelliJ IDE
+brew cask install intellij-idea
 
-# media player
-brew cask install mpv
+# iterm2 terminal
+brew cask install iterm2
 
-# replace search
-brew cask install alfred
+# keeweb password manager
+brew cask install keeweb
+
+# krita - photoshop like
+brew cask install krita
+
+# open office docs
+brew cask install libreoffice
+
+# nicer image viewer
+brew cask install lightgallery
 
 # install little-snitch, check connection out are authorised
 brew cask install little-snitch
 
-# vs code
-brew cask install visual-studio-code
+# media player
+brew cask install mpv
+
+# remove notification when screensharing
+brew cask install muzzle
+
+# path finder explorer (replace finder)
+brew cask install path-finder
+
+# plex client
+brew cask install plex-media-player
+
+# install spectacle window manager
+brew cask install spectacle
+
+# spotify
+brew cask install spotify
+
+# steam
+brew cask install steam
 
 # termhere in path finder or finder
 brew cask install termhere
 
-# realpath needed for the dotfiles install
-brew install coreutils
+# archives 
+brew cask install the-unarchiver
 
-# remove notification when screensharing
-brew cask install muzzle
+# vs code
+brew cask install visual-studio-code
+
+# vivaldi-snapshot browser, just to have another browser in case
+brew cask install vivaldi-snapshot
+
+# vlc media player, just in case mpv doesn't read the video
+brew cask install vlc
+
+# vim editor
+brew install vim
+
+# wavebox messaging app aggregator
+brew cask install wavebox
 
 # Remove brew cruft
 brew cleanup
 
 # Remove cask cruft
 brew cask cleanup 
+
+# NOT INSTALLED AUTOMATICALLY BUT LISTED
+# send videos to Apple TV or chrome cast
+# brew cask install beamer
+
+
+
+# MAS
+printf "Installing apple store application, you will need to be logged in already\n\n"
+
+# install mas - install App Store app from command line
+if test ! $(which brew)
+then
+  echo "  Installing MAS for you, tool to install "
+  brew install mas
+fi
+
+# bear notes - id comes from mas search bear
+mas install 1091189122
+
+# gravit designer
+mas install 1207744923
+
+# John background switcher
+mas install 907640277
 
 # Installing dofiles
 printf "Installing dotfiles\n\n"
